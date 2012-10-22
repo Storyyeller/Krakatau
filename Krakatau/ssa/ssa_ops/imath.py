ddcdd1c2fe9c0453c7856f6ba181cb745e9290f1
Robert Grosse <n210241048576@gmail.com>
2012-10-22 17:43:41 -0400

    Improve bitwise propagation

--- Krakatau/ssa/ssa_ops/imath.py
+++ Krakatau/ssa/ssa_ops/imath.py
@@ -55,17 +55,30 @@ def split_pow2ranges(x,y):
         x += 1<<k
     return out
 
-def propagateBitwise(op, arg1, arg2):
+def propagateBitwise(arg1, arg2, op, usemin, usemax):
     ranges1 = split_pow2ranges(arg1.min, arg1.max)
     ranges2 = split_pow2ranges(arg2.min, arg2.max)
 
     vals = []
     for (s1,k1),(s2,k2) in itertools.product(ranges1, ranges2):
-        s = op(s1, s2)
-        k = max(k1,k2)
-        mask = (1<<k) - 1
-        vals.append(s & ~mask)
-        vals.append(s | mask)
+        # there are three parts. The high bits fixed in both arguments,
+        # the middle bits fixed in one but not the other, and the 
+        # lowest bits which can be chosen freely for both arguments
+        # high = op(h1,h2) and low goes from 0 to 1... but the range of
+        # the middle depends on the particular operation
+        # 0-x, x-1 and 0-1 for and, or, and xor respectively
+        if k1 > k2:
+            (s1,k1),(s2,k2) = (s2,k2),(s1,k1)
+
+        mask1 = (1<<k1) - 1
+        mask2 = (1<<k2) - 1 - mask1
+
+        high = op(s1, s2) & ~(mask1 | mask2)
+        midmin = (s1 & mask2) if usemin else 0
+        midmax = (s1 & mask2) if usemax else mask2
+
+        vals.append(high | midmin)
+        vals.append(high | midmax | mask1)
     return IntConstraint.range(arg1.width, min(vals), max(vals)),
 
 class IAnd(BaseOp):
@@ -74,7 +87,7 @@ class IAnd(BaseOp):
         self.rval = parent.makeVariable(args[0].type, origin=self)
 
     def propagateConstraints(self, x, y):
-        return propagateBitwise(operator.__and__, x, y)
+        return propagateBitwise(x, y, operator.__and__, False, True)
 
 class IOr(BaseOp):
     def __init__(self, parent, args):
@@ -82,7 +95,7 @@ class IOr(BaseOp):
         self.rval = parent.makeVariable(args[0].type, origin=self)
 
     def propagateConstraints(self, x, y):
-        return propagateBitwise(operator.__or__, x, y)
+        return propagateBitwise(x, y, operator.__or__, True, False)
 
 class IXor(BaseOp):
     def __init__(self, parent, args):
@@ -90,7 +103,7 @@ class IXor(BaseOp):
         self.rval = parent.makeVariable(args[0].type, origin=self)
 
     def propagateConstraints(self, x, y):
-        return propagateBitwise(operator.__xor__, x, y)
+        return propagateBitwise(x, y, operator.__xor__, False, False)
 
 #############################################################################################
 class IShl(BaseOp):

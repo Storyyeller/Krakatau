c9a4ae9114e2ddb3db543038593d4dfda1ce1181
Robert Grosse <n210241048576@gmail.com>
2013-01-17 00:44:22 -0500

    Remove unused constant pool functions

--- Krakatau/constant_pool.py
+++ Krakatau/constant_pool.py
@@ -1,4 +1,3 @@
-from __future__ import division
 import struct, collections
 
 identity = lambda x:x
@@ -11,73 +10,60 @@ def strToBytes(args):
     s = encodeStr(args)[0]
     return struct.pack('>H',len(s)) + s
 
-def trim(x, bits):
-    m = 1<<bits
-    x = x % m
-    if x >= m//2:
-        x -= m
-    return x
-
 cpoolInfo_t = collections.namedtuple('cpoolInfo_t',
-                                     ['name','tag','fromArgs','recoverArgs','fromRaw','toBytes'])
+                                     ['name','tag','recoverArgs','fromRaw','toBytes'])
 
 Utf8 = cpoolInfo_t('Utf8',1,
-                  (lambda self,s:(s,)),
                   (lambda self,(s,):(s,)),
                   decodeStr,
                   strToBytes)
 
 Class = cpoolInfo_t('Class',7,
-                    (lambda self,name:(self.Utf8(name),)),
                     (lambda self,(n_id,):self.getArgs(n_id)),
                     identity,
                     (lambda (n_id,): struct.pack('>H',n_id)))
 
 NameAndType = cpoolInfo_t('NameAndType',12,
-                (lambda self,name,desc:(self.Utf8(name),self.Utf8(desc))),
                 (lambda self,(n,d):self.getArgs(n) + self.getArgs(d)),
                 identity,
                 (lambda (n,d): struct.pack('>HH',n,d)))
 
 Field = cpoolInfo_t('Field',9,
-                (lambda self,cls,name,desc:(self.Class(cls),self.NameAndType(name,desc))),
                 (lambda self,(c_id, nat_id):self.getArgs(c_id) + self.getArgs(nat_id)),
                 identity,
                 (lambda (n,d): struct.pack('>HH',n,d)))
 
 Method = cpoolInfo_t('Method',10,
-                (lambda self,cls,name,desc:(self.Class(cls),self.NameAndType(name,desc))),
                 (lambda self,(c_id, nat_id):self.getArgs(c_id) + self.getArgs(nat_id)),
                 identity,
                 (lambda (n,d): struct.pack('>HH',n,d)))
 
 InterfaceMethod = cpoolInfo_t('InterfaceMethod',11,
-                (lambda self,cls,name,desc:(self.Class(cls),self.NameAndType(name,desc))),
                 (lambda self,(c_id, nat_id):self.getArgs(c_id) + self.getArgs(nat_id)),
                 identity,
                 (lambda (n,d): struct.pack('>HH',n,d)))
 
-String = cpoolInfo_t('String',8,(lambda self,name:(self.Utf8(name),)),
+String = cpoolInfo_t('String',8,
                 (lambda self,(n_id,):self.getArgs(n_id)),
                 identity,
                 (lambda (n_id,): struct.pack('>H',n_id)))
 
-Int = cpoolInfo_t('Int',3,(lambda self,val:(trim(val,32),)),
+Int = cpoolInfo_t('Int',3,
                   (lambda self,(s,):(s,)),
                   identity,
                   (lambda (val,): struct.pack('>i',val)))
 
-Long = cpoolInfo_t('Long',5,(lambda self,val:(trim(val,64),)),
+Long = cpoolInfo_t('Long',5,
                   (lambda self,(s,):(s,)),
                   identity,
                   (lambda (val,): struct.pack('>q',val)))
 
-Float = cpoolInfo_t('Float',4,(lambda self,val:(val,)),
+Float = cpoolInfo_t('Float',4,
                   (lambda self,(s,):(s,)),
                   identity,
                   (lambda (val,): struct.pack('>f',val)))
 
-Double = cpoolInfo_t('Double',6,(lambda self,val:(val,)),
+Double = cpoolInfo_t('Double',6,
                   (lambda self,(s,):(s,)),
                   identity,
                   (lambda (val,): struct.pack('>d',val)))
@@ -107,16 +93,6 @@ class ConstPool(object):
         self.pool.append((name, val))
         return len(self.pool)-1
 
-    def getArgs(self, i):
-        if not (i >= 0 and i<len(self.pool)):
-            raise IndexError('Constant pool index {} out of range'.format(i))        
-        if self.pool[i][0] is None:
-            raise IndexError('Constant pool index {} invalid'.format(i))
-        
-        name, val = self.pool[i]
-        t = name2Type[name]
-        return t.recoverArgs(self, val)
-
     # Special function for assembler
     def getItemRaw(self, item):
         if item[0] == 'Utf8':
@@ -131,6 +107,16 @@ class ConstPool(object):
             return i
 
     # Accessors ####################################################################33
+    def getArgs(self, i):
+        if not (i >= 0 and i<len(self.pool)):
+            raise IndexError('Constant pool index {} out of range'.format(i))        
+        if self.pool[i][0] is None:
+            raise IndexError('Constant pool index {} invalid'.format(i))
+        
+        name, val = self.pool[i]
+        t = name2Type[name]
+        return t.recoverArgs(self, val)
+
     def getArgsCheck(self, typen, index):
         assert(self.pool[index][0] == typen)
         val = self.getArgs(index)
@@ -150,17 +136,3 @@ class ConstPool(object):
             parts.append(struct.pack('>B',t.tag))
             parts.append(t.toBytes(vals))
         return ''.join(parts)
-
-    def __str__(self, maxlen=79):
-        def printLn(args):
-            i,(name,val) = args
-            if name == 'Utf8':
-                s = val[0].encode('unicode_escape')
-                return '{}: "{}"'.format(i, s)
-            elif len(val) == 1:
-                return '{}: {}({})'.format(i, name, val[0])
-            else:
-                return '{}: {}{}'.format(i, name, val)
-
-        lines = [printLn(pair)[:maxlen] for pair in self.getEnumeratePoolIter()]
-        return '\n'.join(lines)

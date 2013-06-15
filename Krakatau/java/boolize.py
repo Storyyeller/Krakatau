9af08acab31ebe60089d5fc2dd2a7bef309e5831
Robert Grosse <n210241048576@gmail.com>
2013-06-15 09:51:52 -0700

    Greatly speed up boolization in exchange for lower quality)

--- Krakatau/java/boolize.py
+++ Krakatau/java/boolize.py
@@ -2,45 +2,139 @@ import collections
 
 from ..ssa.objtypes import IntTT, ShortTT, CharTT, ByteTT, BoolTT
 from . import ast
+from .. import graph_util
 
-class _GraphNode(object):
-    def __init__(self, key):
-        self.keys = frozenset([key])
-        self.weights = collections.defaultdict(int)
+def addSourceArray(left, right, arg_vars, varlist, srcvars, bytearrs, boolarrs):
+    if left.dtype[0] == '.bexpr':
+        assert(left not in arg_vars)
+        if left in bytearrs or left in boolarrs:
+            return
+        varlist.append(left) # Note, may have duplicates, but this shouldn't really matter
 
-def graphCutVars(root, arg_vars, visit_cb):
-    # Any var which isn't known to be 0 or 1 is wide and must have a forced value
-    # Forced int if any defs are methods, fields, arrays, or literals > 1 or any
-    # defs are forced int. Forced bool otherwise
+        assert(not isinstance(right, (ast.Assignment, ast.Parenthesis, ast.Ternary)))
+        if right in arg_vars or isinstance(right, 
+            (ast.ArrayCreation, ast.Cast, ast.FieldAccess, ast.MethodInvocation)):
+            temp = bytearrs if right.dtype[0] == ByteTT[0] else boolarrs
+            temp.add(left)
+            srcvars[left] = []
+        elif isinstance(right, ast.Local):
+            srcvars[left].append(right)
+        elif isinstance(right, ast.ArrayAccess):
+            assert(isinstance(right.params[0], ast.Local))
+            srcvars[left].append(right.params[0])
 
-    # Compute graph for 01 vars
-    # choose a cut
-    # assign every chosen var to bool dtype
-    # add expr param casts
-    edges = []
-    nodes = collections.OrderedDict()
-    def get(key):
-        if key not in nodes:
-            nodes[key] = _GraphNode(key)
-        return nodes[key]
+def handleResultsArray(varlist, bytearrs, boolarrs):
+    for var in varlist:
+        assert(var.dtype[1] > 0)
+        if var in bytearrs:
+            var.dtype = ByteTT[0], var.dtype[1]        
+        elif var in boolarrs:
+            var.dtype = BoolTT[0], var.dtype[1]
 
-    def contract(*keys):
-        knodes = map(get, keys)
-        new = knodes[0]
-        new.keys = frozenset().union(*(x.keys for x in knodes))
+_int_tts = IntTT, ShortTT, CharTT, ByteTT, BoolTT
+_int_ops = '* / % + - << >> >>>'.split()
+_bool_ops = ' < > <= >= == != instanceof && ||'.split()
+_any_ops = '& ^ |'.split()
 
-        assert(not (True in new.keys and False in new.keys))
+def addSourceScalar(left, right, arg_vars, varlist, srcvars, ints, bools):
+    if left.dtype in _int_tts:
+        if left in ints or left in bools:
+            return
+        varlist.append(left) # Note, may have duplicates, but this shouldn't really matter
 
-        for key in new.keys:
-            nodes[key] = new
+        if left in arg_vars:
+            temp = bools if right.dtype == BoolTT else ints
+            temp.add(left)
+            return
 
-    def addedge(key1, key2):
-        edges.append((key1, key2))
+        def addLocalLitOrTernary(expr):
+            if isinstance(expr, ast.Local):
+                srcvars[left].append(expr)            
+            elif isinstance(expr, ast.Ternary):
+                addLocalLitOrTernary(expr.params[1])
+                addLocalLitOrTernary(expr.params[2])
+            else:
+                assert(isinstance(expr, ast.Literal))
+                if not 0 <= expr.val <= 1:
+                    ints.add(left)
 
+        assert(not isinstance(right, (ast.Assignment, ast.Parenthesis, ast.UnaryPrefix)))
+        if isinstance(right, (ast.ArrayCreation, ast.Cast, ast.FieldAccess, ast.MethodInvocation, 
+            ast.ArrayAccess)):
+            temp = bools if right.dtype == BoolTT else ints
+            temp.add(left)
+        elif isinstance(right, (ast.Local, ast.Literal, ast.Ternary)):
+            addLocalLitOrTernary(right)
+        elif isinstance(right, ast.BinaryInfix):
+            if right.opstr in _int_ops:
+                ints.add(left)
+            elif right.opstr in _bool_ops:
+                bools.add(left)
+            else:
+                assert(right.opstr in _any_ops)
+                addLocalLitOrTernary(right.params[0])
+                addLocalLitOrTernary(right.params[1])
+
+        if left in bools or left in ints:
+            srcvars[left] = []
+
+def handleResultsScalar(varlist, ints, bools):
+    for var in varlist:
+        if var in bools:
+            var.dtype = BoolTT
+
+def propagateTypes(root, arg_vars, addSource, handleResults):
+    varlist = []
+    srcvars = collections.defaultdict(list)
+    set1 = set()
+    set2 = set()
+
+    def visitAssign(left, right):
+        if isinstance(left, ast.Local):
+            addSource(left, right, arg_vars, varlist, srcvars, set1, set2)
+
+    def visitScope(scope):
+        for item in scope.statements:
+            for sub in item.getScopes():
+                visitScope(sub)
+            if isinstance(item.expr, ast.Assignment):
+                visitAssign(*item.expr.params)
+    visitScope(root)
+
+    temp = set1 | set2
+    unknown = [v for v in varlist if v not in temp and not temp.add(v)]
+
+    ordered = graph_util.topologicalSort(unknown, lambda v:srcvars[v])
+    for var in ordered:
+        for source in srcvars[var]:
+            if source in set1:
+                set1.add(var)
+                break            
+            elif source in set2:
+                set2.add(var)
+                break
+
+    assert(not set2 & set1)
+    assert(not set2 & arg_vars)
+    handleResults(varlist, set1, set2)
+
+def boolizeVars(root, arg_vars):
+    arg_vars = frozenset(arg_vars)
+    propagateTypes(root, arg_vars, addSourceArray, handleResultsArray)    
+    propagateTypes(root, arg_vars, addSourceScalar, handleResultsScalar)  
+
+    #Make sure types match in binary expressions
     def visitExpr(expr):
         for param in expr.params:
             visitExpr(param)
-        visit_cb(expr, arg_vars, addedge, contract)
+
+        if isinstance(expr, (ast.Assignment, ast.BinaryInfix)):
+            left, right = expr.params
+            if left.dtype == BoolTT:
+                right = ast.makeCastExpr(BoolTT, right)
+            elif right.dtype == BoolTT and isinstance(expr, ast.BinaryInfix):
+                left = ast.makeCastExpr(BoolTT, left)
+            expr.params = left, right
 
     def visitScope(scope):
         for item in scope.statements:
@@ -48,135 +142,4 @@ def graphCutVars(root, arg_vars, visit_cb):
                 visitScope(sub)
             if item.expr is not None:
                 visitExpr(item.expr)
-
     visitScope(root)
-    assert(get(False) != get(True))
-    for key1, key2 in edges:
-        node1, node2 = get(key1), get(key2)
-        if node1 != node2:
-            node1.weights[node2] += 1
-            node2.weights[node1] += 1
-
-    temp = set()
-    nodelist = [n for n in nodes.values() if not n in temp and not temp.add(n)]
-    for node in nodelist:
-        items = node.weights.items()
-        node.weights = collections.OrderedDict(sorted(items, key=lambda (n,w):nodelist.index(n)))
-
-    #Greedy algorithm - todo: Edumund Karps
-    set1 = set()
-    stack = [get(False)]
-    while stack:
-        cur = stack.pop()
-        if cur not in set1 and True not in cur.keys:
-            set1.add(cur)
-            stack.extend(cur.weights)
-
-    set2 = temp - set1
-    keys1 = frozenset().union(*(n.keys for n in set1))
-    keys2 = frozenset().union(*(n.keys for n in set2))
-    keys1 -= set([False])
-    keys2 -= set([True])
-    assert(False not in keys2)
-    return keys1, keys2
-
-_prefixes = '.bexpr', BoolTT[0], ByteTT[0]
-def visitExpr_array(expr, arg_vars, addedge, contract):
-    getbase = lambda expr:expr.dtype[0] if expr.dtype else None
-
-    pkeys = [(expr, i) for i,param in enumerate(expr.params) if getbase(param) in _prefixes]
-    for key in pkeys:
-        param = expr.params[key[1]]
-        if not isinstance(param, ast.Literal):
-            addedge(key, param)
-        if getbase(param) != '.bexpr':
-            contract((getbase(param) == BoolTT[0]), param)
-
-    if isinstance(expr, ast.Assignment):
-        if len(pkeys) == 2:
-            addedge(*pkeys)
-            contract(expr, pkeys[0])
-    elif isinstance(expr, ast.ArrayAccess):
-        if pkeys:
-            contract(expr, pkeys[-1])   
-    elif isinstance(expr, (ast.MethodInvocation, ast.ClassInstanceCreation)):
-        for i,tt in enumerate(expr.tts):
-            if tt and tt[0] in _prefixes:
-                contract((tt[0]==BoolTT[0]), (expr,i))
-    elif isinstance(expr, ast.Ternary):
-        if getbase(expr) in _prefixes:
-            contract(expr, *pkeys[1:])    
-
-def processResults_array(bytekeys, boolkeys):
-    for key in bytekeys | boolkeys:
-        if not isinstance(key, tuple):
-            expr = key
-            base, dim = expr.dtype
-            if base == '.bexpr':
-                new = BoolTT[0] if key in boolkeys else ByteTT[0]
-                expr.dtype = new, dim
-
-all_tts = IntTT, ShortTT, CharTT, ByteTT, BoolTT
-def visitExpr_bool(expr, arg_vars, addedge, contract):
-    contractIf = lambda (expr):contract((expr.dtype==BoolTT), expr) if expr.dtype in all_tts else None
-
-    if expr in arg_vars:
-        contractIf(expr)
-
-    pkeys = [(expr, i) for i,param in enumerate(expr.params) if param.dtype in all_tts]
-    for key in pkeys:
-        param = expr.params[key[1]]
-        if not isinstance(param, ast.Literal):
-            addedge(key, param)
-        else:
-            if param.val not in (0,1):
-                contract(False, param)
-
-    if isinstance(expr, ast.Assignment):
-        if len(pkeys) == 2:
-            addedge(*pkeys)
-            contract(expr, pkeys[0])
-    elif isinstance(expr, ast.ArrayAccess):
-        contractIf(expr)
-        contract(False, pkeys[0])     
-    elif isinstance(expr, ast.BinaryInfix):
-        if expr.opstr in ('==', '!=', '<', '>', '<=', '>=', 'instanceof'):
-            contract(True, expr)
-            if pkeys:
-                if expr.opstr in ('==', '!='):
-                    contract(*pkeys)
-                elif expr.opstr in ('<', '>', '<=', '>='):
-                    contract(False, *pkeys)
-        elif expr.opstr in ('+', '/', '*', '-', '%', '<<', '>>', '>>>'):
-            contract(False, expr, *pkeys)
-        elif expr.opstr in ('&', '|', '^'):
-            contract(expr, *pkeys)
-        else:
-            assert(0)
-    elif isinstance(expr, (ast.MethodInvocation, ast.ClassInstanceCreation)):
-        for i,tt in enumerate(expr.tts):
-            if tt in all_tts:
-                contract((tt==BoolTT), (expr,i))
-        contractIf(expr)
-    elif isinstance(expr, ast.Ternary):
-        if expr.dtype in all_tts:
-            contract(expr, *pkeys[1:])
-        contract(True, pkeys[0])        
-    else:
-        contractIf(expr) 
-
-def processResults_bool(intkeys, boolkeys):
-    for key in boolkeys:
-        if not isinstance(key, tuple):
-            expr = key
-            expr.dtype = BoolTT
-    for key in boolkeys:
-        if isinstance(key, tuple):
-            expr, i = key
-            new = ast.makeCastExpr(BoolTT, expr.params[i])
-            assert(new.dtype == BoolTT)
-            expr.params = type(expr.params)((new if j == i else x) for j,x in enumerate(expr.params))
-
-def boolizeVars(root, arg_vars):
-    processResults_array(*graphCutVars(root, arg_vars, visitExpr_array))
-    processResults_bool(*graphCutVars(root, arg_vars, visitExpr_bool))

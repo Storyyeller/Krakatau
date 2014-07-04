7b911fe9f992cfd68307a2b2c062dba8e437ff63
Robert Grosse <n210241048576@gmail.com>
2014-07-03 23:31:27 -0700

    Add test for Procyon to try catch test

--- tests/source/TryCatchTest.java
+++ tests/source/TryCatchTest.java
@@ -2,35 +2,51 @@ import java.nio.channels.*;
 import java.net.*;
 import java.io.*;
 
-public class TryCatchTest{
+public class TryCatchTest {
+    static volatile boolean i2 = true;
 
-	static volatile boolean i2 = true;
-	
+    public static void main(String[] args)
+    {
+        try{
+            int x = args.length;
 
-	public static void main(String[] args)
-	{
-		try{
-			int x = args.length;
+            try{
+                if (args[0].equals("bad") && i2){
+                    throw new MalformedURLException(args[1] + args[1]);
+                }
 
-			try{
-				if (args[0].equals("bad") && i2){
-					throw new MalformedURLException(args[1] + args[1]);
-				}
+                if (args[0].equals("good") || ++x == 3){
+                    throw new FileLockInterruptionException();
+                }
+            } catch (final MalformedURLException e) {
+                throw e;
+            } catch (Exception e) {
+                Throwable t = new MalformedURLException(e.getClass().getName());
+                Throwable t2 = e.initCause(t);
+                throw (MalformedURLException)t;
+            }
 
-				if (args[0].equals("good") || ++x == 3){
-					throw new FileLockInterruptionException();
-				}
-			} catch (final MalformedURLException e) {
-				throw e;
-			} catch (Exception e) {
-				Throwable t = new MalformedURLException(e.getClass().getName());
-				Throwable t2 = e.initCause(t);
-				throw (MalformedURLException)t;
-			}
+            System.out.println(x);
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
 
-			System.out.println(x);
-		} catch (IOException e){
-			System.out.println(e);
-		}
-	}
+    // This function was added because it breaks Procyon 0.5.25
+    public static int bar()
+    {
+        while(true) {
+            ltry:
+            try {
+                main(null);
+                return 0;
+            } catch (Throwable t) {
+                t.printStackTrace();
+                continue;
+            } finally {
+                int x = 0;
+                break ltry;
+            }
+        }
+    }
 }

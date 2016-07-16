c68ff09a6a74e8e1b6e5bc42b1ee523e8d275098
Robert Grosse <n210241048576@gmail.com>
2016-07-16 11:13:20 -0700

    Sanitize long paths on Linux too

--- Krakatau/script_util.py
+++ Krakatau/script_util.py
@@ -38,57 +38,83 @@ def normalizeClassname(name):
     return name.replace('\\','/').replace('.','/')
 
 # Windows stuff
-illegal_win_chars = frozenset('<>;:|?*\\/"')
+illegal_win_chars = frozenset('<>;:|?*\\/"%')
 pref_disp_chars = frozenset('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$0123456789')
 
 # Prevent creating filename parts matching the legacy device filenames. While Krakatau can create these files
 # just fine thanks to using \\?\ paths, the resulting files are impossible to open or delete in Windows Explorer
 # or with similar tools, so they are a huge pain to deal with. Therefore, we don't generate them at all.
-illegal_parts = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8',
-    'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9']
+illegal_parts = frozenset(['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8',
+    'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9'])
 
-def isPartOk(s, prev):
-    if not 1 <= len(s) <= 200:
-        return False
-    if s.upper() in illegal_parts:
-        return False
-    if s.lower() in prev:
-        return prev[s.lower()] == s
-    # avoid collision with hashed parts
-    if len(s) >= 34 and s[-34:-32] == '__':
-        return False
+class PathSanitizer(object):
+    def __init__(self, base, suffix):
+        self.base = base
+        self.suffix = suffix
 
-    if min(s) <= '\x1f' or max(s) >= '\x7f':
-        return False
-    return illegal_win_chars.isdisjoint(s)
+    def is_part_ok(self, s, parents):
+        if not 1 <= len(s) <= self.MAX_PART_LEN:
+            return False
+        # avoid potential collision with hashed parts
+        if len(s) >= 34 and '__' in s:
+            return False
+        # . cannot appear in a valid class name, but might as well specifically exclude these, just in case
+        if s.startswith('.') or '..' in s:
+            return False
+        return '\x1f' < min(s) <= max(s) < '\x7f'
 
-def isPathOk(s, prevs):
-    if len(s) > 32000:
-        return False
-    parts = s.split('/')
-    return 0 < len(parts) < 750
+    def hash(self, s, suffix):
+        left = ''.join(c for c in s if c in pref_disp_chars)
+        right = '__' + hashlib.md5(s.encode('utf8')).hexdigest() + suffix
+        return left[:self.MAX_PART_LEN - len(right)] + right
 
-def sanitizePart(s, suffix, prev):
-    # make sure that suffix is never in any parts so we don't get a collision after adding it
-    if isPartOk(s, prev) and suffix not in s:
-        return s
-    ok = ''.join(c for c in s if c in pref_disp_chars)
-    return ok[:8] + '__' + hashlib.md5(s.encode('utf8')).hexdigest()
+    def sanitize(self, path):
+        oldparts = path.split('/')
+        newparts = []
+        for i, part in enumerate(oldparts):
+            suffix = self.suffix if i + 1 == len(oldparts) else ''
+            if self.is_part_ok(part + suffix, newparts):
+                newparts.append(part + suffix)
+            else:
+                newparts.append(self.hash(part, suffix))
 
-def winSanitizePath(base, suffix, prevs, s):
-    if isPathOk(s, prevs):
-        parts = s.split('/')
-        sparts = [sanitizePart(p, suffix, prevs[i]) for i,p in enumerate(parts)]
-        for i, sp in enumerate(sparts):
-            prevs[i][sp.lower()] = sp
-        path = '\\'.join(sparts)
-    else:
-        path = '__' + hashlib.md5(s.encode('utf8')).hexdigest()
-        prevs[0][path.lower()] = path
-    return '\\\\?\\{}\\{}{}'.format(base, path, suffix)
+        result = self.format_path([self.base] + newparts)
+        if len(result) > self.MAX_PATH_LEN:
+            result = self.format_path([self.base, self.hash(path, self.suffix)])
+        assert result.endswith(self.suffix)
+        return result
 
-def otherMakePath(base, suffix, s):
-    return os.path.join(base, *s.split('/')) + suffix
+class LinuxPathSanitizer(PathSanitizer):
+    MAX_PART_LEN = 255
+    MAX_PATH_LEN = 4095
+
+    def __init__(self, *args):
+        PathSanitizer.__init__(self, *args)
+
+    def format_path(self, parts):
+        return os.path.join(*parts)
+
+class WindowsPathSanitizer(PathSanitizer):
+    MAX_PART_LEN = 255
+    MAX_PATH_LEN = 32000 # close enough
+
+    def __init__(self, *args):
+        PathSanitizer.__init__(self, *args)
+        # keep track of previous paths to detect case-insensitive collisions
+        self.prevs = collections.defaultdict(dict)
+
+    def is_part_ok(self, s, parents):
+        if not PathSanitizer.is_part_ok(self, s, parents):
+            return False
+        if s.upper() in illegal_parts:
+            return False
+        # make sure nothing in the current directory is a case insensitive collision
+        if self.prevs[tuple(parents)].setdefault(s.lower(), s) != s:
+            return False
+        return illegal_win_chars.isdisjoint(s)
+
+    def format_path(self, parts):
+        return '\\\\?\\' + '\\'.join(parts)
 
 class DirectoryWriter(object):
     def __init__(self, base_path, suffix):
@@ -100,10 +126,9 @@ class DirectoryWriter(object):
 
         osname = platform.system().lower()
         if 'win' in osname and 'darwin' not in osname and 'cygwin' not in osname:
-            prevs = collections.defaultdict(dict) # keep track of previous paths to detect case-insensitive collisions
-            self.makepath = partial(winSanitizePath, base_path, suffix, prevs)
+            self.makepath = WindowsPathSanitizer(base_path, suffix).sanitize
         else:
-            self.makepath = partial(otherMakePath, base_path, suffix)
+            self.makepath = LinuxPathSanitizer(base_path, suffix).sanitize
 
     def write(self, cname, data):
         out = self.makepath(cname)
